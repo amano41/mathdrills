aba55fc742be8de24e7463a95fbab7dade117962
AMANO Yoichi <amano.yoichi@gmail.com>
2019-10-28 18:20:36 +0900

    Add _make_2d_2d_carrying()

--- add.py
+++ add.py
@@ -18,6 +18,7 @@ def main():
     f[4] = _make_2d_2d
     f[5] = _make_2d_1d_carrying
     f[6] = _make_2d_2d_both
+    f[7] = _make_2d_2d_carrying
 
     if len(sys.argv) == 1:
         lv = 1
@@ -161,6 +162,41 @@ def _make_2d_2d_both(num=20):
     for ten, one in zip(tens, ones):
         t1 = ten[0] * 10 + one[0]
         t2 = ten[1] * 10 + one[1]
+        ans = t1 + t2
+        eq.append((t1, t2, ans))
+
+    return eq
+
+
+def _make_2d_2d_carrying(num=20):
+    """
+    2 桁 + 2 桁の繰り上がりのある足し算
+    """
+
+    eq = list()
+
+    # 十の位（和が 20 ～ 80 になるペア）
+    tens = list()
+    wt = list()
+
+    for ans in range(2, 9):
+        e = ans // 2 + 1
+        w = 1 / (e - 1)
+        for t1 in range(1, e):
+            t2 = ans - t1
+            tens.append((t1, t2, ans))
+            wt.append(w)
+
+    tens = _sample(tens, wt, num)
+    tens = _swap_random(tens)
+
+    # 一の位
+    ones = _make_1d_1d_carrying(num)
+
+    # 合成
+    for a, b in zip(tens, ones):
+        t1 = a[0] * 10 + b[0]
+        t2 = a[1] * 10 + b[1]
         ans = t1 + t2
         eq.append((t1, t2, ans))
 
